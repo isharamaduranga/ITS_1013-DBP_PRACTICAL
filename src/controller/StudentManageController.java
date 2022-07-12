d9517297d2562f0f39f1bac7fc471b3c53d32e81
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-07-12 10:35:48 +0530

    created  user interface featuers and all needed textFields Buttons And Tables ..

--- src/controller/StudentManageController.java
+++ src/controller/StudentManageController.java
@@ -8,5 +8,49 @@
 
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class StudentManageController {
+    public TableView tblStudent;
+    public TableColumn colStudentID;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtSAddress;
+    public JFXTextField txtNic;
+    public JFXTextField txtSearchStudentId;
+    public JFXButton btnAdd;
+    public JFXButton btnDelete;
+    public JFXButton btnUpdate;
+    public JFXButton btnClear;
+
+    public void textFields_Key_Released(KeyEvent keyEvent) {
+    }
+
+    public void AddStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearFieldsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMouseMovedOnAction(MouseEvent mouseEvent) {
+    }
 }
